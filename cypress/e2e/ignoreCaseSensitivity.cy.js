a03670219c035b2509d00233f353e771d42ec317
Anshita Bhasin <anshita.bhasin@gmail.com>
2023-06-02 00:20:52 +0400

    case insensitivity

---
+++ cypress/e2e/ignoreCaseSensitivity.cy.js
@@ -0,0 +1,11 @@
+describe('Find case sensitive search on the page', () => {
+
+    it('verify exact match case in Cypress', () => {
+        cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=common/home')
+        cy.contains('ADDONS', { matchCase: false }).click()
+
+
+    })
+
+
+});
